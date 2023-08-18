36283441c53b7704bd74e84703c45e8b75d63cad
I3orborygmos <117126600+I3orborygmos@users.noreply.github.com>
2023-08-17 19:45:02 -0700

    Add files via upload

---
+++ React Fun Facts/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+    <main>
+        <h1 className="main--title">Fun Facts about React</h1>
+        <ul>
+        <li>Was First Reasled in 2013</li>
+        <li>Was orignially created by Jordan Walke</li>
+        <li>Has Well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebood</li>
+        <li>Powers thousands of enterprise apps including, mobile apps</li>
+        </ul>
+    </main>
+    )
+}
---
+++ React Fun Facts/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+       <nav>
+           <img src="./images/react-icon-small.png" />
+            <h3 className="nav--logo"> ReactFacts</h3>
+            <h4 className="nav--text">React Course - Project 1</h4>
+                
+        </nav>
+    )
+}
